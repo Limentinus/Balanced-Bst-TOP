f219a777376db63a4228e1b14aeb72cc76ad3191
LughsClues <cuberfreak@gmail.com>
2023-02-20 15:19:27 +0100

    create Tree class

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -6,3 +6,15 @@ class Node
     @right = right
   end
 end
+
+class Tree
+  def initialize(array)
+    @array = array
+    root =
+  end
+
+  def build_tree(array)
+    sorted_array = array.uniq.sort
+    p sorted_array
+  end
+end
